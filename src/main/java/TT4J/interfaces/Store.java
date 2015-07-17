81d7df8d64a7b897bf255aee7e398ef5b72d2714
1o0ko <wojciech.stokowiec@gmail.com>
2015-07-17 13:15:18 +0200

    - Added docs

--- src/main/java/TT4J/interfaces/Store.java
+++ src/main/java/TT4J/interfaces/Store.java
@@ -6,5 +6,12 @@ import java.io.IOException;
  * Created by stokowiec on 2015-07-17.
  */
 public interface Store {
+    /**
+     * Interface used in LinkProvider which should be implemented
+     * by any class that generates URI of the active link
+     * @param activeLink active link to be stored
+     * @return  URI of the active link
+     * @throws IOException
+     */
     String storeLink(String activeLink) throws IOException;
 }
